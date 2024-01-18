8c1f64bd835ef0ae2efb9bb07544150ad0544d2e
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-01-18 13:00:23 -0300

    Reconstruindo o projeto usando o styled-components

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import Cores from '../../Cores'
+
+export const Header = styled.header`
+  background-color: ${Cores.corSecundaria};
+  color: ${Cores.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
